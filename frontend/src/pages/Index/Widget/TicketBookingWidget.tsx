7e5a49ee89a30dd414b1e3038120e7aafe9d296a
Improver2108 <yashrawat2108@gmail.com>
2022-09-19 03:18:51 +0530

    outsde click switch betwn ele & i/p's

--- frontend/src/pages/Index/Widget/TicketBookingWidget.tsx
+++ frontend/src/pages/Index/Widget/TicketBookingWidget.tsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const TicketBookingWidget=()=>{
-    return(
+const TicketBookingWidget = () => {
+
+    const fromDestination = useRef<any>(null);
+    const toDestionation = useRef<any>(null);
+
+    const [fromDestinationClick, setFromDestinationClick] = useState<boolean>(false);
+    const [toDestinationClick, setToDestinationClick] = useState<boolean>(false);
+
+    const fromHandleOutsideClick = (e: any) => {
+        if (fromDestination.current && !fromDestination.current.contains(e.target)) {
+            setFromDestinationClick(prevDest => false);
+        }
+    }
+    const toHandleOutsideClick = (e: any) => {
+        if (toDestionation.current && !toDestionation.current.contains(e.target)) {
+            setToDestinationClick(prevDest => false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('click',fromHandleOutsideClick, true);
+        document.addEventListener('click',toHandleOutsideClick, true);
+        return () => {
+            document.removeEventListener('click',fromHandleOutsideClick, true);
+            document.removeEventListener('click',toHandleOutsideClick, true);
+        }
+    }, [fromDestinationClick,toDestinationClick])
+
+
+    const fromDestinationButton = <>
+        <span className="mini-text">DEL</span>
+        <span className="main-text">NEW DELHI</span>
+    </>;
+
+    const destinationInput = <>
+        <input type="text" />
+    </>;
+
+    const toDestinationButton = <>
+        <span className="mini-text">BOM</span>
+        <span className="main-text">MUMBAI</span>
+    </>
+
+
+    return (
         <div className="booking-widget">
             <div className="from-destination-container">
-                <div className="destination">
-                    <span className="mini-text">DEL</span>
-                    <span className="main-text">NEW DELHI</span>
+                <div ref={fromDestination} className="destination" onClick={e=>setFromDestinationClick(i=>true)}>
+                    {!fromDestinationClick ? fromDestinationButton : destinationInput}
                 </div>
             </div>
             <div className="to-destination-container">
-                <div className="destination">
-                    <span className="mini-text">BOM</span>
-                    <span className="main-text">MUMBAI</span>
+                <div ref={toDestionation} className="destination" onClick={e=>setToDestinationClick(i=>true)}>
+                    {!toDestinationClick ? toDestinationButton : destinationInput}
                 </div>
             </div>
             <div className="dates">
@@ -28,9 +69,9 @@ const TicketBookingWidget=()=>{
                 <span>CLASS</span>
             </div>
             <div className="details-submission">
-                <button>Search</button>
-            </div>            
-        </div> 
+                <button type="submit">Search</button>
+            </div>
+        </div>
     )
 };
 export default TicketBookingWidget;
