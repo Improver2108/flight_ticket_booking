ea650968ff668ed1a499c0d5242c074dc5bb3150
Improver2108 <yashrawat2108@gmail.com>
2022-09-21 02:53:51 +0530

    addition of slider buttoin in calendar

--- frontend/src/pages/Index/Widget/Calendar.tsx
+++ frontend/src/pages/Index/Widget/Calendar.tsx
@@ -5,7 +5,12 @@ const Calendar =()=>{
 
     return (
         <div className="calendar-container">
-            <h1>Calender</h1>
+            <button className="left-slider">
+                <div className="left-slider-layout"></div>
+            </button>
+            <button className="right-slider">
+                <div className="right-slider-layout"></div>
+            </button>
         </div>
     );
 }
