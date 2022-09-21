b30a863029c1e8c169682510c89a811e65c0df84
Improver2108 <yashrawat2108@gmail.com>
2022-09-22 05:19:44 +0530

    calendar layout creation

--- frontend/src/pages/Index/Widget/Calendar.tsx
+++ frontend/src/pages/Index/Widget/Calendar.tsx
@@ -10,9 +10,17 @@ const Calendar =()=>{
             </button>
             <button className="right-slider">
                 <div className="right-slider-layout"></div>
-            </button>         
+            </button>
+            <div className="calendar-layout">
+                <div className="inner-calendar-layout">
+                    <div className="calendar-dates">month1</div>
+                    <div className="calendar-dates">month1</div>                    
+                </div>
+            </div>
+            <div className="price-information">
+                <p>Fares indicate price for 1 Adult and Economy Class in INR and are subject to change</p>      
+            </div>         
         </div>
     );
 }
-
 export default Calendar 
