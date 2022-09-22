3b367749714f509b551919b575b4c3a2951ddf56
Improver2108 <yashrawat2108@gmail.com>
2022-09-23 01:33:43 +0530

    months and weeks using luxon

--- frontend/src/pages/Index/Widget/Calendar.tsx
+++ frontend/src/pages/Index/Widget/Calendar.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { DateTime } from "luxon";
 import './Calendar.css'
 
-const Calendar =()=>{
-
+const Calendar = () => {
+    // var mL:string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // var mS:string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    const lisWeekdays: any = ["MON", "TUE", "WED", "THU", "FRI"].map(weekday => <div className="weekdays">{weekday}</div>);
+    const lisWeekends: any = ["SAT", "SUN"].map(weekend => <div className="weekends">{weekend}</div>);
+    let date = DateTime.now();
     return (
         <div className="calendar-container">
             <button className="left-slider">
@@ -13,13 +18,33 @@ const Calendar =()=>{
             </button>
             <div className="calendar-layout">
                 <div className="inner-calendar-layout">
-                    <div className="calendar-dates">month1</div>
-                    <div className="calendar-dates">month1</div>                    
+                    <div className="calendar-dates">
+                        <div className="month-name">
+                            <p>{date.toLocaleString({ month: "long", year: "numeric" })}</p>
+                        </div>
+                        <div className="weeks">
+                            {lisWeekdays}
+                            {lisWeekends}
+                        </div>
+                        <div className="day-number">
+                            
+                        </div>
+                    </div>
+
+                    <div className="calendar-dates">
+                        <div className="month-name">
+                            <p>{date.plus({ months: 1 }).toLocaleString({ month: "long", year: "numeric" })}</p>
+                        </div>
+                        <div className="weeks">
+                            {lisWeekdays}
+                            {lisWeekends}
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="price-information">
-                <p>Fares indicate price for 1 Adult and Economy Class in INR and are subject to change</p>      
-            </div>         
+                <p>Fares indicate price for 1 Adult and Economy Class in INR and are subject to change</p>
+            </div>
         </div>
     );
 }
